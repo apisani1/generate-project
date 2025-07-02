7f05b1e4fb05a97609f595ad589ef8c3329185be
Antonio Pisani <antonio.pisani@gmail.com>
2025-07-02 19:16:41 -0400

    release 0.1.0: feat(minor)
    Changes
    --------------------------------------------------------------------------------
    - First version

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ from datetime import datetime
 project = "generate-project"
 copyright = f"{datetime.now().year}, Antonio Pisani"
 author = "Antonio Pisani"
-release = "0.0.0"
+release = "0.1.0"
 
 os.environ["SPHINX_BUILD"] = "True"
 
