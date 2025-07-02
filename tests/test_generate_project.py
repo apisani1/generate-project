c63432f5a753623d123b8f4f0ffd7de15e3380ba
Antonio Pisani <antonio.pisani@gmail.com>
2025-07-02 17:25:00 -0400

    First commit

--- tests/test_generate_project.py
+++ tests/test_generate_project.py
@@ -1,12 +1,5 @@
 """Basic tests for generate-project."""
 
-
-def test_import() -> None:
-    """Test that the package can be imported."""
-    import generate_project
-    assert hasattr(generate_project, '__version__')
-
-
 def test_version() -> None:
     """Test that version is defined."""
     from generate_project import __version__
