c0d471662164d95b461715a04b117930498141b2
Antonio Pisani <antonio.pisani@gmail.com>
2025-07-07 00:00:39 -0400

    Fix module import issue when running tests locally

--- src/generate_project/templates/poetry-template/{{cookiecutter.project_name}}/tests/conftest.py
+++ src/generate_project/templates/poetry-template/{{cookiecutter.project_name}}/tests/conftest.py
@@ -7,5 +7,10 @@ from pathlib import Path
 THIS_DIR = Path(__file__).parent
 TESTS_DIR_PARENT = (THIS_DIR / "..").resolve()
 
-# ensure that `from tests ...` import statements work within the tests/ dir
+# Ensure that `from tests ...` import statements work within the tests/ dir
 sys.path.insert(0, str(TESTS_DIR_PARENT))
+
+# Add src directory to path to ensure package can be importe
+src_dir = TESTS_DIR_PARENT / "src"
+if src_dir.exists():
+    sys.path.insert(0, str(src_dir))
